a68f3881a08d8de43dcb29801f93a75a94f564a9
Fabian Zeiher <fzeiher@gmail.com>
2017-12-17 14:27:05 +0100

    DOCS: comments complete

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -5,18 +5,22 @@ public final class Constants{
     private Constants() {
 
     }
-
+    // Canvas Constants
     public static final int CANVAS_WIDTH = 1280;
     public static final int CANVAS_HEIGHT = 960;
     public static final int FRAME_RATE = 60;
 
+    ///////////////////////////////////////////////////////////
+    // Image Constants
     // original by robertsspaceindustries.com
     public static final String PLAYER_IMAGE_PATH = "./data/assets/ship_bucc_low.png";
     public static final int PLAYER_IMAGE_WIDTH = 120;
     public static final int PLAYER_IMAGE_HEIGHT = 105;
+    // found at http://pixelartmaker.com/art/dabdcb017b41820
     public static final String COLLISION_IMAGE_PATH = "./data/assets/collision.png";
     public static final int COLLISION_IMAGE_WIDTH = 855;
     public static final int COLLISION_IMAGE_HEIGHT = 330;
+    //original found at "http://ajournalofmusicalthings.com/winners-of-nordic-prize-scandinavia-and-the-meteor-prize-ireland-announced/"
     public static final String YOU_WIN_IMAGE_PATH = "./data/assets/youwin.png";
     public static final int YOU_WIN_IMAGE_WIDTH = 606;
     public static final int YOU_WIN_IMAGE_HEIGHT = 80;
@@ -28,6 +32,8 @@ public final class Constants{
     // original by robertsspaceindustries.com
     public static final String MAIN_MENU_IMAGE = "./data/assets/main_menu.png";
 
+    ///////////////////////////////////////////////////////////
+    // Input Constants
     public static final int PLAYER_UP_INPUT = 1;
     public static final int PLAYER_LEFT_INPUT = 2;
     public static final int PLAYER_DOWN_INPUT = 3;
@@ -35,12 +41,18 @@ public final class Constants{
     public static final int PLAYER_RESET_INPUT = 5;
     public static final int PLAYER_PAUSE_INPUT = 6;
 
+    ///////////////////////////////////////////////////////////
+    // Player Constants
     public static final int PLAYER_START_X = CANVAS_WIDTH/2 - PLAYER_IMAGE_WIDTH /2;
     public static final int PLAYER_START_Y = CANVAS_HEIGHT - 200;
 
+    ///////////////////////////////////////////////////////////
+    // Level Constants
     public static final int LEVEL_LENGTH = 100;
     public static final int MAX_LEVEL_NUM = 17;
 
+    ///////////////////////////////////////////////////////////
+    // Level Design Constants
     public static final int OBSTACLE_MIN_SIZE = 30;
     public static final int OBSTACLE_MAX_SIZE = 90;
     public static final int VIRTUAL_GRID_HEIGHT = 120;
@@ -49,10 +61,14 @@ public final class Constants{
     public static final int VIRTUAL_GRID_ROW_NUM = 5;
     public static final int VIRTUAL_GRID_ROW_SPACING = VIRTUAL_GRID_HEIGHT * 2;
 
+    ///////////////////////////////////////////////////////////
+    // String Constants
     public static final String TOOL_TIP_COLLISION = "press [space] to restart level";
     public static final String TOOL_TIP_END_SCREEN = "press [space] to restart game";
     public static final String TOOL_TIP_GAME_PAUSED = "press [space] to continue";
 
+    ///////////////////////////////////////////////////////////
+    // Enumeration Constants
     public enum Gamemode {
         challenge,
         endlessLevel
